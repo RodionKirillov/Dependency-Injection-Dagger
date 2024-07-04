bf94fdcb1a129e33a5cd7e2f685f239008a922f3
RodionKirillov <rodion.44454@gmail.com>
2024-07-04 17:26:41 +0300

    Binds. Linking interface and implementation

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/repository/ExampleRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.dependencyinjectionstart.example2.data.repository
+
+import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLocalDataSource
+import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSource
+import com.example.dependencyinjectionstart.example2.domain.ExampleRepository
+import javax.inject.Inject
+
+class ExampleRepositoryImpl @Inject constructor(
+    private val localDataSource: ExampleLocalDataSource,
+    private val remoteDataSource: ExampleRemoteDataSource
+) : ExampleRepository {
+
+    override fun method() {
+        localDataSource.method()
+        remoteDataSource.method()
+    }
+}
