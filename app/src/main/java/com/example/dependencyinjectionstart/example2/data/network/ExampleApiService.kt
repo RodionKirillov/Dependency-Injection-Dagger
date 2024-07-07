24f0e71a6944f5ba63fa61abb1fdeb7f8773eb81
RodionKirillov <rodion.44454@gmail.com>
2024-07-07 14:49:17 +0300

    Custom Builder, BindsInstance

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -6,11 +6,15 @@ import com.example.dependencyinjectionstart.R
 import javax.inject.Inject
 
 class ExampleApiService @Inject constructor(
-    private val context: Context
+    private val context: Context,
+    private val timeMillis: Long
 ) {
 
     fun method() {
-        Log.e(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)}")
+        Log.e(
+            LOG_TAG,
+            "ExampleApiService ${context.getString(R.string.app_name) + " " + timeMillis.toString()}"
+        )
     }
 
     companion object {
