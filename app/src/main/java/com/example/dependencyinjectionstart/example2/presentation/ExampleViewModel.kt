c62662b4d513a86a7b2034dc605275fafc642a1e
RodionKirillov <rodion.44454@gmail.com>
2024-07-09 17:10:04 +0300

    Add Qualifiers

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ExampleViewModel.kt
@@ -2,17 +2,21 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.dependencyinjectionstart.example2.di.IdQualifier
+import com.example.dependencyinjectionstart.example2.di.NameQualifier
 import com.example.dependencyinjectionstart.example2.domain.ExampleUseCase
 import javax.inject.Inject
+import javax.inject.Named
 
 class ExampleViewModel @Inject constructor(
     private val useCase: ExampleUseCase,
-    private val id: String
-): ViewModel() {
+    @IdQualifier private val id: String,
+    @NameQualifier private val name: String
+) : ViewModel() {
 
     fun method() {
         useCase.invoke()
-        Log.e(LOG_TAG, this.toString() + id)
+        Log.e(LOG_TAG, this.toString() + id + name)
     }
 
     companion object {
--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     private val component by lazy {
         (application as ExampleApplication).component
             .activityComponentFactory()
-            .create("MY_ID")
+            .create("MY_ID", "MY_NAME")
     }
 
 
